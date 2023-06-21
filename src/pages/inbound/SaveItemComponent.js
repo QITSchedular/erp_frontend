d0c76aa199b4a3834b1ca866041ec5550aabdce0
QITSchedular <qitschedular2023@gmail.com>
2023-06-21 10:23:59 +0530

    save from

--- src/pages/inbound/SaveItemComponent.js
+++ src/pages/inbound/SaveItemComponent.js
@@ -1,11 +1,19 @@
-import React from "react";
-import { Form, GroupItem, SimpleItem, Label } from "devextreme-react/form";
-
+import React, { useState } from "react";
+import {
+  Form,
+  GroupItem,
+  SimpleItem,
+  ButtonItem,
+  ButtonOptions,
+} from "devextreme-react/form";
+// Updated import statement
 import "devextreme-react/text-area";
 
-import { employee, positions, states } from "./data.js";
+import { positions, states } from "./data.js";
 
 const SaveItemComponent = () => {
+  const [formData, setFormData] = useState({}); // State to store the form data
+
   const birthDateOptions = { width: "100%" };
   const positionOptions = {
     items: positions,
@@ -17,8 +25,17 @@ const SaveItemComponent = () => {
   const phoneOptions = { mask: "+1 (000) 000-0000" };
   const notesOptions = { height: 140 };
 
-  const avatarRender = () => {
-    return <div className="form-avatar"></div>;
+  const handleSubmit = () => {
+    // Handle the form data here
+    console.log(formData);
+  };
+
+  const handleFormChange = (e) => {
+    // Update the form data state on form field change
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.dataField]: e.value,
+    }));
   };
 
   return (
@@ -26,7 +43,7 @@ const SaveItemComponent = () => {
       <h2 className={"content-block"}>All Items</h2>
       <div className={"content-block"}>
         <div className={"dx-card responsive-paddings"}>
-          <Form formData={employee}>
+          <Form formData={formData} onFieldDataChanged={handleFormChange}>
             <GroupItem cssClass="first-group" colCount={4}>
               <GroupItem colSpan={3}>
                 <SimpleItem dataField="Item Group Name" />
@@ -34,6 +51,9 @@ const SaveItemComponent = () => {
                 <SimpleItem dataField="Locked" />
               </GroupItem>
             </GroupItem>
+            <ButtonItem horizontalAlignment="left">
+              <ButtonOptions text="Save" onClick={handleSubmit} />
+            </ButtonItem>
           </Form>
         </div>
       </div>
